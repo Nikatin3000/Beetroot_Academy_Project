7f0859cec20fb218b5ced5a1d8531b14936daae5
Vadim Kryuk <vadimkryuk145@gmail.com>
2023-07-18 11:43:06 +0300

    ver0.3

--- tel_bot.py
+++ tel_bot.py
@@ -18,45 +18,59 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def search_books(update: Update, context: CallbackContext) -> None:
     tags = context.args
-    if tags:
-        url = api_url + 'search-books/'
-        data = {'tags': tags}
+    if not tags:
+        message = "Пожалуйста, укажите теги книг для поиска."
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return
 
-        async with aiohttp.ClientSession() as session:
+    url = api_url + 'search-books/'
+    data = {'tags': tags}
+
+    async with aiohttp.ClientSession() as session:
+        try:
             async with session.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'}) as response:
                 results = await response.json()
 
-        if 'results' in results:
-            message = "Результаты поиска:\n"
-            for book in results['results']:
-                message += f"Название: {book['title']}, Автор: {book['author']}\n"
-        else:
-            message = "Книги с такими тегами не найдены."
-    else:
-        message = "Пожалуйста, укажите теги книг для поиска."
+            if 'results' in results:
+                message = "Результаты поиска:\n"
+                for book in results['results']:
+                    message += f"Название: {book['title']}, Автор: {book['author']}\n"
+            else:
+                message = "Книги с такими тегами не найдены."
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        except Exception as e:
+            logging.error(f"Ошибка при выполнении запроса поиска книг: {e}")
+            message = "Произошла ошибка при выполнении запроса поиска книг. Пожалуйста, попробуйте еще раз позже."
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 async def mark_as_read(update: Update, context: CallbackContext) -> None:
-    if len(context.args) >= 2:
-        book_id = context.args[0]
-        user_id = context.args[1]
+    if len(context.args) < 2:
+        message = "Пожалуйста, укажите идентификатор книги и идентификатор пользователя."
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return
 
-        url = api_url + 'mark-as-read/'
-        data = {'book_id': book_id, 'user_id': user_id}
+    book_id, user_id = context.args[:2]
 
-        async with aiohttp.ClientSession() as session:
+    url = api_url + 'mark-as-read/'
+    data = {'book_id': book_id, 'user_id': user_id}
+
+    async with aiohttp.ClientSession() as session:
+        try:
             async with session.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'}) as response:
                 result = await response.json()
 
-        if 'status' in result and result['status'] == 'success':
-            message = "Книга отмечена как прочитанная."
-        else:
-            message = "Не удалось отметить книгу как прочитанную."
-    else:
-        message = "Пожалуйста, укажите идентификатор книги и идентификатор пользователя."
+            if 'status' in result and result['status'] == 'success':
+                message = "Книга отмечена как прочитанная."
+            else:
+                message = "Не удалось отметить книгу как прочитанную."
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        except Exception as e:
+            logging.error(f"Ошибка при отметке книги как прочитанной: {e}")
+            message = "Произошла ошибка при отметке книги как прочитанной. Пожалуйста, попробуйте еще раз позже."
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
 
 
 if __name__ == '__main__':
